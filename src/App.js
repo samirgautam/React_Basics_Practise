38a06417bb3632beb70bbeb4bd913241f646cb03
samirgautam <samirgautam003@gmail.com>
2023-04-07 17:30:45 +0545

    02 useCase hook

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
 
 import './App.css';
-import {User} from './User.js';
+import { useState } from 'react';
 
 function App() {
-  const users = [
-    { name: "John" , age: 28},
-    { name: "Jane", age: 27},
-    { name: "Jim", age: 26},
-    { name: "Jill", age: 25},
-  ];
-  const planets = [
-  { name: "mars", isGasPlanet : false },
-  { name: "earth", isGasPlanet : false },
-  { name: "venus", isGasPlanet : false },
-  { name: "jupiter", isGasPlanet : true },
-  { name: "neptune", isGasPlanet : true },
-  { name: "uranus", isGasPlanet : true },
-  ];
+const [age, setAge] = useState(0);
 
+const increaseAge = () =>{
+  setAge(age + 1);
+}
 
-  return ( 
+  return (
     <div className = "App">
-
-      {users.map((user,key)=>{
-        return <User name= {user.name} age= {user.age} />;
-      })}
-
-      {planets.map((planet,key) => {
-      if(planet.isGasPlanet)  return <h1>{planet.name}</h1> ;
-      })
-      }
+{age} <button onClick ={increaseAge}>Increase Age</button>
+      
     </div>
   );
 }
