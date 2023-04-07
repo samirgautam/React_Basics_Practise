0d5dd5302525ff184dc53717c39685df73af6d44
samirgautam <samirgautam003@gmail.com>
2023-04-07 15:33:34 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {User} from './User.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const users = [
+    { name: "John" , age: 28},
+    { name: "Jane", age: 27},
+    { name: "Jim", age: 26},
+    { name: "Jill", age: 25},
+  ];
+  const planets = [
+  { name: "mars", isGasPlanet : false },
+  { name: "earth", isGasPlanet : false },
+  { name: "venus", isGasPlanet : false },
+  { name: "jupiter", isGasPlanet : true },
+  { name: "neptune", isGasPlanet : true },
+  { name: "uranus", isGasPlanet : true },
+  ];
+
+
+  return ( 
+    <div className = "App">
+
+      {users.map((user,key)=>{
+        return <User name= {user.name} age= {user.age} />;
+      })}
+
+      {planets.map((planet,key) => {
+      if(planet.isGasPlanet)  return <h1>{planet.name}</h1> ;
+      })
+      }
     </div>
   );
 }
