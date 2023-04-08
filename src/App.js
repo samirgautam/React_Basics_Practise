4bef0a2e9def71ee94dd73f9ae308640cf344e2e
samirgautam <samirgautam003@gmail.com>
2023-04-08 15:20:34 +0545

    start file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 
 import './App.css';
-import { useState } from 'react';
+
 
 function App() {
-const [age, setAge] = useState(0);
-
-const increaseAge = () =>{
-  setAge(age + 1);
-}
 
   return (
     <div className = "App">
-{age} <button onClick ={increaseAge}>Increase Age</button>
-      
+      Hello World !
     </div>
   );
 }
