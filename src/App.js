48fca5243cc2179f342896039fef41e2a401ecc6
samirgautam <samirgautam003@gmail.com>
2023-04-08 15:18:06 +0545

    crud-todoList

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 
 import './App.css';
 import { useState } from 'react';
+import {Task} from "./Task";
 
-function App() {
-const [age, setAge] = useState(0);
+function App(){
+const [todoList, setTodoList] = useState([]);
+const [newTask, setNewTask] = useState("");
 
-const increaseAge = () =>{
-  setAge(age + 1);
+const handleChange = (event) =>{
+  setNewTask(event.target.value);
 }
+const addTask =() => {
+  const task ={
+    id:todoList.length === 0 ? 1: todoList[todoList.length-1].id + 1,
+    taskName: newTask,
+    completed: false
+  };
+  setTodoList([...todoList , task]);
+};
+const deleteTask = (id) => {
+  setTodoList(todoList.filter((task)=> task.id !== id));
+};
 
-  return (
-    <div className = "App">
-{age} <button onClick ={increaseAge}>Increase Age</button>
-      
+const completeTask = (id) =>{
+ setTodoList(
+  todoList.map((task) => {
+    if(task.id === id ){
+    return {...task , completed :true };
+    }else{
+      return task;
+    }
+  })
+ );
+};
+
+  return(
+    <div className='App'>
+      <div className='addTask'>
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className='list'>
+      {todoList.map((task) => {
+        return (<Task taskName = {task.taskName} id = {task.id} completed = {task.completed} deleteTask ={deleteTask} completeTask ={completeTask}/>);
+      })}
+      </div>
     </div>
   );
 }
