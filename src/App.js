6967dab03d6bd58d57dfa9694d5635eafb335d2b
samirgautam <samirgautam003@gmail.com>
2023-04-10 19:16:56 +0545

    useContext

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { Home } from './pages/Home';
-import { Menu } from './pages/Menu';
+import { Profile } from './pages/Profile';
 import { Contact } from './pages/Contact';
 import { Navbar } from './pages/Navbar';
-
+import {useState, createContext} from "react";
+export const  AppContext = createContext();
 
 function App() {
-
+  const [username, setUsername] = useState("SamirTech");
   return (
     <div className = "App">
+      <AppContext.Provider value={{username,setUsername}}>
       <Router>
         <Navbar />
         <Routes>  
           <Route path='/' element={<Home />}/>
-          <Route path="/menu" element = {<Menu />} />
+          <Route path="/profile" element = {<Profile />} />
           <Route path ="/contact" element = { <Contact />} />
           <Route path='*' element ={ <h1>Page Not Found</h1>} />
         </Routes>
       </Router>
+      </AppContext.Provider>
     </div>
   );
 }
