5fb5e73d40b7a0213b916e6b13125d4fe8dce1ae
samirgautam <samirgautam003@gmail.com>
2023-04-10 06:41:46 +0545

    day3-react-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Home } from './pages/Home';
+import { Menu } from './pages/Menu';
+import { Contact } from './pages/Contact';
+import { Navbar } from './pages/Navbar';
 
 
 function App() {
 
   return (
     <div className = "App">
-      Hello World !
+      <Router>
+        <Navbar />
+        <Routes>  
+          <Route path='/' element={<Home />}/>
+          <Route path="/menu" element = {<Menu />} />
+          <Route path ="/contact" element = { <Contact />} />
+          <Route path='*' element ={ <h1>Page Not Found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
