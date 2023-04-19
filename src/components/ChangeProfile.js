2158961cd6abc11e6e79877c01a897b3c4fa2325
samirgautam <samirgautam003@gmail.com>
2023-04-19 14:04:11 +0545

    updated change profile

--- src/components/ChangeProfile.js
+++ src/components/ChangeProfile.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useContext } from "react";
 import { AppContext } from "../App";
-export const ChangeProfile = (props) => {
+export const ChangeProfile = () => {
    const { setUsername } = useContext(AppContext);
 
     const [newUsername,setNewUsername] = useState("");
