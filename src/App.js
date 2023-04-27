f19fd4ef830ab65c3e54d2d67505179b25efd0c6
samirgautam <samirgautam003@gmail.com>
2023-04-27 15:54:57 +0545

    day5-reactquery

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import { Home } from './pages/Home';
 import { Menu } from './pages/Menu';
 import { Contact } from './pages/Contact';
 import { Navbar } from './pages/Navbar';
-
+import { QueryClient , QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
-
+const client = new QueryClient({
+  defaultOptions: {
+      queries:{
+        refetchtOnWindowFocus: false,
+      }
+  }
+}); 
   return (
     <div className = "App">
+      <QueryClientProvider client={client}>
       <Router>
         <Navbar />
         <Routes>  
@@ -20,6 +27,7 @@ function App() {
           <Route path='*' element ={ <h1>Page Not Found</h1>} />
         </Routes>
       </Router>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
 export const Home =() =>{
-     return <h1>This is the Home Page</h1>
+     const { data:catData, isLoading, refetch } = useQuery(["cat"], ()=>{
+          return Axios.get("https://catfact.ninja/fact").then((res)=> res.data);
+     });
+     if(isLoading){
+          return <h1>Loading ... </h1>
+     }
+
+     return( 
+     <h1>This is the Home Page <p>{catData?.fact}</p>
+     <button onClick={refetch}>Update Data</button>
+     </h1>);
+     
 };
