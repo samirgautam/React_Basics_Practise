1e2795d06bd4b3e6ae71a8f5abe28452112eac91
samirgautam <samirgautam003@gmail.com>
2023-04-27 17:29:07 +0545

    day6-reactForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-
+import { Form } from "./components/Form";
 
 function App() {
 
   return (
     <div className = "App">
-      Hello World !
+      <Form />
     </div>
   );
 }
